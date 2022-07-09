7dcf78a62decda1de32f77204689397353275098
Maycon Braga <maycondbraga.s@gmail.com>
2022-07-09 17:36:11 -0300

    Add project files.

---
+++ TesteExcel/Program.cs
@@ -0,0 +1,39 @@
+﻿using ClosedXML.Excel;
+using System;
+using System.Linq;
+
+namespace TesteExcel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var xls = new XLWorkbook(@"C:\Users\Maycon\Downloads\MOCK_DATA.xlsx");
+            var planilha = xls.Worksheets.First(w => w.Name == "data");
+            var totalLinhas = planilha.Rows().Count();
+
+            // primeira linha é o cabecalho
+            for (int l = 2; l <= totalLinhas; l++)
+            {
+                var codigo = planilha.Cell($"A{l}").Value.ToString();
+                var descricao = planilha.Cell($"B{l}").Value.ToString();
+                var preco = planilha.Cell($"C{l}").Value.ToString();
+                Console.WriteLine($"{codigo} - {descricao} - {preco}");
+            }
+
+            Console.WriteLine($"\nSEGUNDA ABA ----------------------------------------\n");
+
+            var planilha2 = xls.Worksheets.First(w => w.Name == "data2");
+            var totalLinhas2 = planilha.Rows().Count();
+
+            // primeira linha é o cabecalho
+            for (int l = 2; l <= totalLinhas2; l++)
+            {
+                var codigo = planilha2.Cell($"A{l}").Value.ToString();
+                var descricao = planilha2.Cell($"B{l}").Value.ToString();
+                var preco = planilha2.Cell($"C{l}").Value.ToString();
+                Console.WriteLine($"{codigo} - {descricao} - {preco}");
+            }
+        }
+    }
+}
